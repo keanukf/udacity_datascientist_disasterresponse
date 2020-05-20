b3219a151c9d2c779d83b1f64a622df67f465f2c
Keanu <forthmann@gmx.net>
2020-05-20 17:27:40 +0200

    Update train_classifier.py
    Docstring and in-line comment additions

--- disaster_response_project/models/train_classifier.py
+++ disaster_response_project/models/train_classifier.py
@@ -25,9 +25,9 @@ import sys
 
 
 def load_data(database_filepath):
-    """ 
+    """
     Load data from database and create feature and label vectors
-    
+
     Args:
       database_filepath (str): filepath where Sqlite DB is located
     Returns:
@@ -35,30 +35,30 @@ def load_data(database_filepath):
       Y (pandas.DataFrame): Disaster Response categories as dataframe. Used as labels
       category_names (list): label names for response categories
     """
-    
+
     # load data from database
     engine = create_engine('sqlite:///'+database_filepath)
-    df = pd.read_sql_table('disaster_messages', engine) 
+    df = pd.read_sql_table('disaster_messages', engine)
     X = df.message.values
     Y = df.iloc[:,4:].values
     category_names = df.iloc[:,4:].columns
-    
+
     return X, Y, category_names
 
 
 def tokenize(text):
-    """ 
+    """
     Tokenizes and Lemmatizes a given text
-    
+
     Args:
       text (str): Text to tokenize
     Returns:
       list: List of text tokens
     """
-    
-    # remove punctiation
+
+    # remove punctuation
     text = re.sub(r'[^a-zA-Z0-9]', " ", text)
-        
+
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -73,37 +73,37 @@ def tokenize(text):
 def build_model():
     """
     Build machine learning pipeline for disaster response tweets
-    
+
     Returns:
-      sklearn.model_selection.GridSearchCV: ML model (and pipeline) to build NLP model 
-                                            using GridSearchCV and DecisionTreeClassifier  
-    """    
+      sklearn.model_selection.GridSearchCV: ML model (and pipeline) to build NLP model
+                                            using GridSearchCV and DecisionTreeClassifier
+    """
 
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(estimator=DecisionTreeClassifier(), n_jobs=-1)),
          ])
-    
-    
+
+
     parameters = {
         'vect__ngram_range': ((1, 1), (1, 2)),
         'vect__max_df': (0.5, 0.75, 1.0),
         'vect__max_features': (None, 5000, 10000),
         'tfidf__use_idf': (True, False),
-        'clf__estimator__max_features': ['log2', None]        
+        'clf__estimator__max_features': ['log2', None]
         #'clf__estimator__n_estimators': [50, 100, 200],
         #'clf__estimator__min_samples_split': [2, 3, 4]
     }
 
     cv = GridSearchCV(pipeline, param_grid=parameters)
-    
+
     return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
     Y_pred = model.predict(X_test)
-    
+
     for i in range(len(y_pred[0,:])):
         print(category_names[i] + ":")
         print(classification_report(Y_test[:,i],Y_pred[:,i]))
@@ -120,13 +120,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
