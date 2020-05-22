5e84ebc4b95477982afba4ede26662397461b6b0
Keanu <forthmann@gmx.net>
2020-05-22 19:50:53 +0200

    Changes in run.py
    Fixed missing Database and Tablename paramters

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -25,9 +25,9 @@ import sys
 
 
 def load_data(database_filepath):
-    """ 
+    """
     Load data from database and create feature and label vectors
-    
+
     Args:
       database_filepath (str): filepath where Sqlite DB is located
     Returns:
@@ -35,34 +35,36 @@ def load_data(database_filepath):
       Y (pandas.DataFrame): Disaster Response categories as dataframe. Used as labels
       category_names (list): label names for response categories
     """
-    
+
     # load data from database
     engine = create_engine('sqlite:///'+database_filepath)
-    df = pd.read_sql_table('disaster_messages', engine) 
+    df = pd.read_sql_table('disaster_messages', engine)
     X = df['message'].iloc[:100].values
     Y = df.iloc[:100,4:].values
     category_names = df.iloc[:100,4:].columns
-    
+
     return X, Y, category_names
 
 
 def tokenize(text):
-    """ 
+    """
     Tokenizes and Lemmatizes a given text
-    
+
     Args:
       text (str): Text to tokenize
     Returns:
       list: List of text tokens
     """
-    
+
     # remove punctiation
     text = re.sub(r'[^a-zA-Z0-9]', " ", text)
-     
-    # tokenize and lemmatize given text    
+
+    # tokenize given text
     tokens = word_tokenize(text)
+    # instantiate lemmatizer
     lemmatizer = WordNetLemmatizer()
 
+    # lemmatize each token
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
@@ -74,36 +76,39 @@ def tokenize(text):
 def build_model():
     """
     Build machine learning pipeline for disaster response tweets
-    
-    Returns:
-      sklearn.model_selection.GridSearchCV: ML model (and pipeline) to build NLP model 
-                                            using GridSearchCV and DecisionTreeClassifier  
-    """    
 
+    Returns:
+      sklearn.model_selection.GridSearchCV: Multioutput ML model (and pipeline) to build NLP model
+                                            using GridSearchCV and DecisionTreeClassifier
+    """
+
+    # build pipeline model
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize, min_df=0.0001)),
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(estimator=DecisionTreeClassifier(), n_jobs=-1)),
          ])
-    
+
+    # set parameters for hyperparameter tuning
     parameters = {
         'vect__ngram_range': ((1, 1), (1, 2)),
         'vect__max_df': (0.5, 0.75, 1.0),
         'vect__max_features': (None, 5000, 10000),
         'tfidf__use_idf': (True, False),
-        'clf__estimator__max_features': ['log2', None],        
+        'clf__estimator__max_features': ['log2', None],
         'clf__estimator__min_samples_split': [2, 3, 4]
     }
 
+    # tune hyperparamters using Gridsearch
     cv = GridSearchCV(pipeline, param_grid=parameters)
-    
+
     return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    """ 
+    """
     Evaluate model performance
-    
+
     Args:
       model (sklearn.model_selection.GridSearchCV): Trained ML model
       X_test (pandas.DataFrame): Test feature set, here these are disaster messages.
@@ -114,26 +119,27 @@ def evaluate_model(model, X_test, Y_test, category_names):
     Returns:
         None
     """
-    
+
     # classify disaster messages using test set
     Y_pred = model.predict(X_test)
-    
+
     # performance report for each prediction of category
     for i in range(len(Y_pred[0,:])):
         print(category_names[i] + ":")
         print(classification_report(Y_test[:,i],Y_pred[:,i]))
- 
+
 
 def save_model(model, model_filepath):
-    """ 
-    Save model as python joblib object to disk 
-    
+    """
+    Save model as python joblib object to disk
+
     Args:
       model (sklearn.model_selection.GridSearchCV): Trained ML model
       model_dilepath (str): filepath to save pickled ML model
     Returns:
       None
     """
+    
     # save the model to disk
     joblib.dump(model, open('{}'.format(model_filepath), 'wb'))
 
@@ -144,13 +150,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
